16958418eedd48aa2d17302721e50fbb910cd6ee
SiracHuseynov <tu6uvrrlv@code.edu.az>
2024-03-11 02:23:42 +0400

    flowchart added

--- taskLoop/taskLoop/Program.cs
+++ taskLoop/taskLoop/Program.cs
@@ -28,40 +28,40 @@ namespace taskLoop
             #endregion
 
             #region 2.ci tapsirig
-            //Verilmis ededin sade ve ya murekkeb oldugunu tapan alqoritm. 
-            Console.WriteLine("Ededi daxil edin: ");
-            int eded = Convert.ToInt16(Console.ReadLine());  
-            bool f = true;
+            //Verilmis ededin sade ve ya murekkeb oldugunu tapan alqoritm.
+            //Console.WriteLine("Ededi daxil edin: ");
+            //int eded = Convert.ToInt16(Console.ReadLine());
+            //bool f = true;
 
 
-            if (eded < 0)
-            {
-                Console.WriteLine("Duzgun deyer daxil edin");
-            }
-            else if (eded == 0 || eded == 1)
-            {
-                Console.WriteLine($"{eded} ne sadedir, nede murekkeb");
-            }
-            else
-            {
-                for (int i = 2; i < eded; i++)
-                {
-                    if (eded % i == 0)
-                    {
-                        f = false;
-                        break;
-                    }
+            //if (eded < 0)
+            //{
+            //    Console.WriteLine("Duzgun deyer daxil edin");
+            //}
+            //else if (eded == 0 || eded == 1)
+            //{
+            //    Console.WriteLine($"{eded} ne sadedir, nede murekkeb");
+            //}
+            //else
+            //{
+            //    for (int i = 2; i < eded; i++)
+            //    {
+            //        if (eded % i == 0)
+            //        {
+            //            f = false;
+            //            break;
+            //        }
 
-                }
-                if (f)
-                {
-                    Console.WriteLine("Eded sadedir!");
-                }
-                else
-                {
-                    Console.WriteLine("Eded murekkebdir!");
-                }
-            }
+            //    }
+            //    if (f)
+            //    {
+            //        Console.WriteLine("Eded sadedir!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Eded murekkebdir!");
+            //    }
+            //}
 
 
             #endregion
@@ -96,7 +96,7 @@ namespace taskLoop
             #endregion
 
             #region 4.cu tapsirig
-            // Verilmiş ədədin polindrom olub olmadığını tapan alqoritm(Meselen: 1221->polindrom, 1231->polindrom deyil)(optional) 
+            //Verilmiş ədədin polindrom olub olmadığını tapan alqoritm(Meselen: 1221->polindrom, 1231->polindrom deyil)(optional)
             //Console.WriteLine("Ededi daxil edin: ");
             //int eded = Convert.ToUInt16(Console.ReadLine());
             //int qaliq;
